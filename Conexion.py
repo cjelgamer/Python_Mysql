150fcd11a28a2e5bd49ad294be3f43abd9dc6e02
cjelgamercc <cjelgamercc@gmail.com>
2024-08-27 17:26:18 -0500

    conexion con la base de datos en mysql

---
+++ Conexion.py
@@ -0,0 +1,23 @@
+#python -m pip install mysql-connector-python
+import mysql.connector 
+
+class CConexion:
+    
+    def ConexionBaseDeDatos():
+        try:
+            
+            conexion = mysql.connector.connect(user = 'root',password='',
+                                               host='127.0.0.1',
+                                               database='peliculadb',
+                                               port='3306')
+            
+            print("Conexion Correcta")
+            
+            return conexion
+            
+        except mysql.connector.Error as error:
+            print("Error al conectarte a la base de Datos {}".format(error))
+            
+            return conexion
+        
+    ConexionBaseDeDatos()
