78285d7f5b719fb357f55a3fa8d01418532f68d4
cjelgamercc <cjelgamercc@gmail.com>
2024-09-01 21:39:29 -0500

    me obliga

--- python_mysql.py
+++ python_mysql.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
-from PIL import Image, ImageTk  # Importar PIL para manejar imágenes
+from PIL import Image, ImageTk
 from peliculas import *
 from Conexion import *
 
